eb5ff2e195c5897f3661c7bb2cd88d99bc3be744
Stella Redler <stella.redler@elev.ga.ntig.se>
2024-03-11 12:37:26 +0100

    update 2

--- game.rb
+++ game.rb
@@ -1,27 +1,34 @@
-require 'ruby2d'
+require 'ruby2d';
 
-set width: 640
-set height: 480
+width = 640
+height = 480
 
-image.new('nedladdning.gif')
+Image.new('Adventures of Pip.png.jfif');
 
-square = square.new(
-    color: 'red',
-    x: 40,
-    y: 10,
-    width: 100,
-    height: 100,
+hooded = Sprite.new(
+    'Hooded.png',
+    x: 200, y: 0, z: 5,
+    width: 103,
+    height: 103,
+    clip_width: 103,
+    time: 200,
+    animations: {
+        idle: 0..1,
+        walk: 2..5,
+    }
 )
 
 on :key_held do |event|
     case event.key
-    when 'up'
-        square.y -= 5
-    when 'down'
-        square.y += 5
-    when 'left'
-        square.x -= 5
-    when 'right'
-        square.x += 5
+        when 'w'
+            hooded.y -= 5
+        when 's'
+            hooded.y += 5
+        when 'a'
+            hooded.x -= 5
+        when 'd'
+            hooded.x += 5
     end
+end
+
 show
