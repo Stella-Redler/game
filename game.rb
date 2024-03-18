8077ecc81cb834195b95a559faf7e977fe72554f
Stella Redler <stella.redler@elev.ga.ntig.se>
2024-03-18 10:48:29 +0100

    update 3

--- game.rb
+++ game.rb
@@ -7,27 +7,33 @@ Image.new('Adventures of Pip.png.jfif');
 
 hooded = Sprite.new(
     'Hooded.png',
-    x: 200, y: 0, z: 5,
-    width: 103,
-    height: 103,
-    clip_width: 103,
+    x: 10, y: 245, z: 5,
+    width: 80,
+    height: 80,
+    clip_width: 54,
     time: 200,
     animations: {
         idle: 0..1,
         walk: 2..5,
+        run: 6..14, 
+        jump: 15..22,
     }
 )
 
 on :key_held do |event|
     case event.key
-        when 'w'
-            hooded.y -= 5
-        when 's'
-            hooded.y += 5
         when 'a'
-            hooded.x -= 5
+            hooded.x -= 1
+            hooded.play flip: :horizontal, animation: :walk
         when 'd'
-            hooded.x += 5
+            hooded.x += 1
+            hooded.play animation: :walk
+        when 'space'
+            hooded.y += -1
+            hooded.play animation: :jump
+        when 'f'
+            hooded.x += 2
+            hooded.play animation: :run
     end
 end
 
