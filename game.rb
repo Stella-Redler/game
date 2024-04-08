c75602f6b0bc74b2b24bd7f36cce3e69fcebbeac
Stella Redler <stella.redler@elev.ga.ntig.se>
2024-04-08 12:30:49 +0200

    update 6

--- game.rb
+++ game.rb
@@ -5,9 +5,10 @@ height = 480
 
 Image.new('Adventures of Pip.png.jfif');
 
+
 hooded = Sprite.new(
     'Hooded_emotes.png',
-    x: 10, y: 250, z: 5,
+    x: 10, y: 260, z: 5,
     width: 80,
     height: 80,
     clip_width: 68,
@@ -15,7 +16,7 @@ hooded = Sprite.new(
     animations: {
         walk: 0..3,
         run: 4..11, 
-        jump: 12..19,
+        jump: 12..18,
     }
 )
 
@@ -28,7 +29,7 @@ on :key_held do |event|
             hooded.x += 1
             hooded.play animation: :walk
         when 'space'
-            hooded.y += -1
+            hooded.y += -9
             hooded.play animation: :jump
         when 'right'
             hooded.x += 1.5
@@ -36,6 +37,17 @@ on :key_held do |event|
         when 'left'
             hooded.x -= 1.5
             hooded.play animation: :run, flip: :horizontal
+    end
+end
+
+arrayY = 260
+
+update do
+    if hooded.y < arrayY
+        hooded.y += 60/(arrayY[hooded.x] - hooded.y + 0.1)
+    end
+    if hooded.y > arrayY[hooded.x]
+        hooded.y = arrayY[hooded.x]
     end
 end
 
